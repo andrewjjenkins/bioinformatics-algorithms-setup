b9d8e109e1ecdcced4fbdcca5975239b738a6c89
Andrew Jenkins <andrewjjenkins@gmail.com>
2023-01-19 23:50:57 +0000

    Add first config files

---
+++ jupyter_lab_config.py
@@ -0,0 +1,6 @@
+# To use a password instead of a token, uncomment the next line and set the
+# password.
+#c.ServerApp.password = ''
+
+# To allow connections from anywhere in your network, uncomment the next line.
+#c.ServerApp.ip = '*'
